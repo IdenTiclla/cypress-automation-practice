9d15f7b1cd4b2e65afe58fe293b204f9b8c82e36
IdenTiclla <iden.ticlla@gmail.com>
2023-12-18 17:03:11 -0400

    best practices beforeEach

--- cypress/e2e/spec-2.cy.js
+++ cypress/e2e/spec-2.cy.js
@@ -3,14 +3,30 @@ describe('My test suite', () => {
     // beforeEach(() => {
     //     cy.visit('https://www.saucedemo.com/v1/')
     // })
+
+
+    beforeEach(() => {
+        cy.visit("https://webdriveruniversity.com/Click-Buttons/index.html")
+    })
+
     Cypress.on('uncaught:exception', (err, runnable) => {
         console.log(err);
         return false;
     })
+    
+    it ('Test 0 - Page Title', () => {
+        cy.title().should('eq', 'WebDriver | Button Clicks')
+    })
 
-    it('Web element click', () => {
-        cy.visit("https://webdriveruniversity.com/Click-Buttons/index.html")
+    it('Test 1 - Page content', () => {
         cy.get("div#main-header > h1").should('have.text', 'Lets Get Clicking!')
+        cy.contains("Lets Get Clicking!")
+        cy.contains("WebdriverUniversity.com (Button Clicks)")
+
+    })
+    it('Test 2 - Three components', () => {
+        cy.get('div.col-sm-4.col-lg-4.col-md-4').should('have.length', 3)
+        cy.get("span[type='button']").should('have.length', 3)
     })
 })
 
