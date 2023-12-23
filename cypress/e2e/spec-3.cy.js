3a7be37e4792e9081d04f68876dd53e99279bed0
IdenTiclla <iden.ticlla@gmail.com>
2023-12-23 07:49:16 -0400

    adding basic input field behavior

--- cypress/e2e/spec-3.cy.js
+++ cypress/e2e/spec-3.cy.js
@@ -3,9 +3,10 @@ describe('My test suite', () => {
         cy.visit("https://webdriveruniversity.com/To-Do-List/index.html")
     })
 
-    it.only("Checking default", () => {
+    it("Checking default", () => {
         cy.get("h1").contains("TO-DO LIST")
         cy.get("input").should("have.attr", 'placeholder', 'Add new todo')
+        cy.get("input").should('be.visible')
         cy.get("i.fa.fa-plus").should('be.visible')
     })
 
@@ -17,5 +18,9 @@ describe('My test suite', () => {
         cy.get("ul li").should('have.length', 3)
     })
     
-    
+    it.only("Test 2 - Basic input field behavior", () => {
+        cy.get("input").should('be.visible')
+        cy.get("i.fa.fa-plus").click()
+        cy.get("input").should('not.be.visible')
+    })
 })
