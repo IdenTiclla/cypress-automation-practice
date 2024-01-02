5d42493db6f254f80ce81d91ae0f823cf43ff390
IdenTiclla <iden.ticlla@gmail.com>
2024-01-02 14:42:05 -0400

    adding test first accordion

--- cypress/e2e/spec-4.cy.js
+++ cypress/e2e/spec-4.cy.js
@@ -3,7 +3,17 @@ describe('My test suite 4', () => {
         cy.visit("https://webdriveruniversity.com/Accordion/index.html")
     })
     
-    it.only("Test 0 - accordions page", () => {
+    it("Test 0 - accordions page", () => {
         cy.title().should('eq', 'Accordion Items')
+    })
+
+    it.only("Test 1 - First accordion", () => {
+        // cy.get("div#manual-testing-description").should('not.be.visible')
+
+        cy.get("button#manual-testing-accordion").should('have.attr', 'class').and('not.contain', 'active')
+        cy.get("button#manual-testing-accordion").click()
+
+        cy.get("button#manual-testing-accordion").should('have.attr', 'class').and('contain', 'active')
+        cy.get("div#manual-testing-description").should('be.visible')
     })
 })
