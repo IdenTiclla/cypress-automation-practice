73bcb405f8f97e41d4545fe1d99a21f16118bd00
IdenTiclla <iden.ticlla@gmail.com>
2024-01-02 18:51:52 -0400

    adding test second accordion

--- cypress/e2e/spec-4.cy.js
+++ cypress/e2e/spec-4.cy.js
@@ -7,7 +7,7 @@ describe('My test suite 4', () => {
         cy.title().should('eq', 'Accordion Items')
     })
 
-    it.only("Test 1 - First accordion", () => {
+    it("Test 1 - First accordion", () => {
         // cy.get("div#manual-testing-description").should('not.be.visible')
 
         cy.get("button#manual-testing-accordion").should('have.attr', 'class').and('not.contain', 'active')
@@ -15,5 +15,14 @@ describe('My test suite 4', () => {
 
         cy.get("button#manual-testing-accordion").should('have.attr', 'class').and('contain', 'active')
         cy.get("div#manual-testing-description").should('be.visible')
+    })
+    it.only("Test 2 - Second accordion", () => {
+        cy.get("button#cucumber-accordion").should('have.attr', 'class').and('not.contain', 'active')
+        cy.get("button#cucumber-accordion").click()
+
+        cy.get("button#cucumber-accordion").should('have.attr', 'class').and('contain', 'active')
+        cy.get("button#cucumber-accordion").should('be.visible')
+
+        cy.get("div#cucumber-testing-description p").should('have.text', 'Cucumber (BDD) simplifies the requirement capturing process. Requirements can be captured, broken down and simplified effortlessly; making the captured requirements readable to anyone within the organisation and in turn providing the required details and backbone to develop accurate test cases also known as ‘Feature Files’.')
     })
 })
