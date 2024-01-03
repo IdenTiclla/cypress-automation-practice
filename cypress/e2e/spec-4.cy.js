90bd1aa41ceea94db7b6b511a6fbc40bc4460ee7
IdenTiclla <iden.ticlla@gmail.com>
2024-01-03 09:06:17 -0400

    adding test for the third accordion

--- cypress/e2e/spec-4.cy.js
+++ cypress/e2e/spec-4.cy.js
@@ -7,7 +7,7 @@ describe('My test suite 4', () => {
         cy.title().should('eq', 'Accordion Items')
     })
 
-    it.only("Test 1 - First accordion", () => {
+    it("Test 1 - First accordion", () => {
         // cy.get("div#manual-testing-description").should('not.be.visible')
 
         cy.get("button#manual-testing-accordion").should('have.attr', 'class').and('not.contain', 'active')
@@ -26,5 +26,12 @@ describe('My test suite 4', () => {
         cy.get("button#cucumber-accordion").should('be.visible')
 
         cy.get("div#cucumber-testing-description p").should('have.text', 'Cucumber (BDD) simplifies the requirement capturing process. Requirements can be captured, broken down and simplified effortlessly; making the captured requirements readable to anyone within the organisation and in turn providing the required details and backbone to develop accurate test cases also known as ‘Feature Files’.')
+    })
+    it.only("Test 3 - Third accordion", () => {
+        cy.get("button#automation-accordion").should('have.attr', 'class').and('not.contain', 'active')
+        cy.get("button#automation-accordion").click()
+
+        cy.get("button#automation-accordion").should('have.attr', 'class').and('contain', 'active')
+        cy.get("div#automation-testing-description p").should('have.text', 'Automation testing has been steadily grown in popularity these past few years thanks to the time/ cost savings and efficiency that it offers. Companies throughout the world have or plan to use automation testing to rapidly speed up their test capabilities. Automation test engineers are in great demand and offer an average salary of £45,000+ (2018). Now is a great time to learn about automation test engineering and this course has been carefully developed to slowly introduce you from the basics, all the way to building advanced frameworks.')
     })
 })
