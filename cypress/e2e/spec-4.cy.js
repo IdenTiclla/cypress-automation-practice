0973d92deedcb28c8a0e8b509678d5a7846e8be4
IdenTiclla <iden.ticlla@gmail.com>
2024-01-03 09:55:53 -0400

    adding test for last accordion component

--- cypress/e2e/spec-4.cy.js
+++ cypress/e2e/spec-4.cy.js
@@ -27,11 +27,22 @@ describe('My test suite 4', () => {
 
         cy.get("div#cucumber-testing-description p").should('have.text', 'Cucumber (BDD) simplifies the requirement capturing process. Requirements can be captured, broken down and simplified effortlessly; making the captured requirements readable to anyone within the organisation and in turn providing the required details and backbone to develop accurate test cases also known as ‘Feature Files’.')
     })
-    it.only("Test 3 - Third accordion", () => {
+    it("Test 3 - Third accordion", () => {
         cy.get("button#automation-accordion").should('have.attr', 'class').and('not.contain', 'active')
         cy.get("button#automation-accordion").click()
 
         cy.get("button#automation-accordion").should('have.attr', 'class').and('contain', 'active')
         cy.get("div#automation-testing-description p").should('have.text', 'Automation testing has been steadily grown in popularity these past few years thanks to the time/ cost savings and efficiency that it offers. Companies throughout the world have or plan to use automation testing to rapidly speed up their test capabilities. Automation test engineers are in great demand and offer an average salary of £45,000+ (2018). Now is a great time to learn about automation test engineering and this course has been carefully developed to slowly introduce you from the basics, all the way to building advanced frameworks.')
+    })
+    it.only("Test 4 - Last accordion component", () => {
+        cy.contains("LOADING.. PLEASE WAIT..").should('be.visible')
+        
+        cy.contains('LOADING COMPLETE.',{ timeout: 10000 }).should('be.visible')
+        cy.get("button#click-accordion").should('have.attr', 'class').and('not.contain', 'active')
+        cy.get("button#click-accordion").click()
+        cy.get("button#click-accordion").should('have.attr', 'class').and('contain', 'active')
+
+        cy.get("div#timeout").should('have.text', 'This text has appeared after 5 seconds!')
+
     })
 })
