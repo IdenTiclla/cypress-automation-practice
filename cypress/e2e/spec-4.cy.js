43776d33c0609208032d757094201654af036703
IdenTiclla <iden.ticlla@gmail.com>
2024-01-04 17:08:09 -0400

    modifying default test

--- cypress/e2e/spec-4.cy.js
+++ cypress/e2e/spec-4.cy.js
@@ -3,8 +3,11 @@ describe('My test suite 4', () => {
         cy.visit("https://webdriveruniversity.com/Accordion/index.html")
     })
     
-    it("Test 0 - accordions page", () => {
+    it.only("Test 0 - accordions page", () => {
         cy.title().should('eq', 'Accordion Items')
+        cy.contains("WebDriver (Accordion Items & Text Appear)")
+        cy.contains("Click on One of the Accordian Items Below!")
+        cy.contains("Copyright © www.GianniBruno.com")
     })
 
     it("Test 1 - First accordion", () => {
@@ -34,7 +37,7 @@ describe('My test suite 4', () => {
         cy.get("button#automation-accordion").should('have.attr', 'class').and('contain', 'active')
         cy.get("div#automation-testing-description p").should('have.text', 'Automation testing has been steadily grown in popularity these past few years thanks to the time/ cost savings and efficiency that it offers. Companies throughout the world have or plan to use automation testing to rapidly speed up their test capabilities. Automation test engineers are in great demand and offer an average salary of £45,000+ (2018). Now is a great time to learn about automation test engineering and this course has been carefully developed to slowly introduce you from the basics, all the way to building advanced frameworks.')
     })
-    it.only("Test 4 - Last accordion component", () => {
+    it("Test 4 - Last accordion component", () => {
         cy.contains("LOADING.. PLEASE WAIT..").should('be.visible')
         
         cy.contains('LOADING COMPLETE.',{ timeout: 10000 }).should('be.visible')
