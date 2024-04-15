90a1a16322f6373faeae3cdee1d3ccd039e29b57
IdenTiclla <iden.ticlla@gmail.com>
2024-04-15 11:13:11 -0400

    adding test for dropdowns - one time

--- cypress/e2e/spec-6.cy.js
+++ cypress/e2e/spec-6.cy.js
@@ -2,7 +2,7 @@ describe("Dropdowns, checkboxes and radio buttons", () => {
     beforeEach(() => {
         cy.visit("https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html")
     })
-    it.only("Test 1 - Default values on dropdowns", () => {
+    it("Test 1 - Default values on dropdowns", () => {
         cy.get("#dropdowm-menu-1").should('have.value', 'java')
         cy.get("#dropdowm-menu-2").should('have.value', 'eclipse')
         cy.get("#dropdowm-menu-3").should('have.value', 'html')
@@ -12,5 +12,14 @@ describe("Dropdowns, checkboxes and radio buttons", () => {
         cy.get("input[value='option-2']").should('not.be.checked')
         cy.get("input[value='option-3']").should('be.checked')
         cy.get("input[value='option-4']").should('not.be.checked')
+    })
+    it.only("Test 2 - Testing dropdowns one time", () => {
+        cy.get("#dropdowm-menu-1").select("c#")
+        cy.get("#dropdowm-menu-2").select("maven")
+        cy.get("#dropdowm-menu-3").select("javascript")
+
+        cy.get("#dropdowm-menu-1").should('have.value', 'c#')
+        cy.get("#dropdowm-menu-2").should('have.value', 'maven')
+        cy.get("#dropdowm-menu-3").should('have.value', 'javascript')
     })
 })
