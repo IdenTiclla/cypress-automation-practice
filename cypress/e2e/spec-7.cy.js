f2c948c5c0ad5489f4042471c6c98d1d8891c569
IdenTiclla <iden.ticlla@gmail.com>
2024-04-25 14:08:52 -0400

    adding test for testing drag and drop functionality

--- cypress/e2e/spec-7.cy.js
+++ cypress/e2e/spec-7.cy.js
@@ -3,11 +3,15 @@ describe("actions test suite", () => {
         cy.visit("https://webdriveruniversity.com/Actions/index.html")
     })
 
-    it.only("Test 0 - Basic structure of the page", () => {
+    it("Test 0 - Basic structure of the page", () => {
         cy.contains("WebdriverUniversity.com (Actions)")
         cy.contains("The Key to Success is to take massive ACTION!")
         cy.contains("Copyright © www.GianniBruno.com")
         
-        // cy.get("div#draggable").drag("div#droppable", {force: true})        
+    })
+    it.only("Test 1 - Testing drag and drop", () => {
+        cy.get("div#droppable > p > b").should('have.text', 'DROP HERE!')     
+        cy.get("div#draggable").drag("div#droppable", {force: true})
+        cy.get("div#droppable > p > b").should('have.text', 'Dropped!')     
     })
 })
