c66599f5681ea739abb161665df5f55253e801b0
IdenTiclla <iden.ticlla@gmail.com>
2024-04-25 21:08:39 -0400

    editing file with vim editor

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Test suite edited with vim', () => {
+  it('Test case edited with vim', () => {
     cy.visit('https://example.cypress.io')
   })
 })
