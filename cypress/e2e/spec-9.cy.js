b8d9bd9bb7816b1ec1594afe584528e01827cc88
IdenTiclla <iden.ticlla@gmail.com>
2024-05-07 11:56:34 -0400

    Modal popup test

--- cypress/e2e/spec-9.cy.js
+++ cypress/e2e/spec-9.cy.js
@@ -16,11 +16,22 @@ describe('Test suite for testing popups and alerts', () => {
         cy.get("div#main-header ~ div[class*='col-sm'] span").should('have.length', 4)
 
     })
-    it.only('Test 1 - Javascript alert test', () => {
+    it('Test 1 - Javascript alert test', () => {
         cy.get('span#button1').click()
         cy.on('window:alert',(t)=>{
             //assertions
             expect(t).to.contains('I am an alert box!');
         })  
+    })
+    it.only('Test 2 - Modal Popup', () => {
+        cy.get("div#myModal > div").should('not.be.visible')
+        cy.get('span#button2').click()
+        cy.get("div#myModal > div").should('be.visible')
+        cy.get("div#myModal  div.modal-header h4").should('have.text', 'It’s that Easy!!  Well I think it is.....')
+        cy.get("div#myModal  div.modal-body p").should('have.text', 'We can inject and use JavaScript code if all else fails! Remember always try to use WebDriver Library method(s) first such as WebElement.click(). (The Selenium development team have spent allot of time developing WebDriver functions etc).')
+
+        cy.get('#myModal > div > div > div.modal-footer > button').click()
+        cy.get("div#myModal > div").should('not.be.visible')
+
     })
 })
