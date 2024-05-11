b6283e54e24f8146c1134df989180ff7fad86cef
IdenTiclla <iden.ticlla@gmail.com>
2024-05-11 15:18:29 -0400

    adding some elements to homepage and updating test case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,23 +4,32 @@ import Login from "../pages/Login"
 const loginPage = new Login()
 const HomePage = new Home()
 const shoppingCartModal = new ShoppingCartModal()
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
+});
+
 describe('Test suite edited with vim', () => {
   
   it('Test case edited with vim', () => {
     cy.visit('https://example.cypress.io')
   })
-  it('Testing sausage page', () => {
+  it.only('Testing sausage page', () => {
     HomePage.visit()
     HomePage.getHomeOption().click()
-    cy.wait(3000)
     HomePage.getCartIconButton().click()
-    cy.wait(3000)
-    // shoppingCartModal.getCloseButton().click()
-    // cy.wait(10000)
+
     shoppingCartModal.closeButton()
     shoppingCartModal.validateMessageForEmptyCart('Your shopping cart is empty!')
+    
+    HomePage.getHomeOption().click()
+    
+    HomePage.getSpecialHotOption().click()
+
+    HomePage.getBlogOption().click()
+
   })
-  it.only('Testing wish list with not logged user', () => {
+  it('Testing wish list with not logged user', () => {
     HomePage.visit()
     HomePage.getWishListIconButton().click()
     cy.wait(3000)
--- cypress/pages/Home.js
+++ cypress/pages/Home.js
@@ -4,6 +4,10 @@ class Home {
     }
     getHomeOption = () => cy.get("ul.horizontal a[href*='common/home']")
     getSpecialHotOption = () => cy.get("ul.horizontal a[href*='product/special']")
+    getBlogOption = () => cy.get("ul.horizontal a[href*='blog/home']")
+    getMegaMenuOption = () => cy.get("ul.horizontal li:nth-of-type(4) a[data-toggle='dropdown']")
+    getAddOnsOption = () => cy.get("ul.horizontal li:nth-of-type(5) a[data-toggle='dropdown']")
+    getMyAccountOption = () => cy.get("ul.horizontal li:nth-of-type(6) a[data-toggle='dropdown']")
     getWishListIconButton = () => cy.get("a[aria-label='Wishlist']")
     getCartIconButton = () => cy.get("div.cart-icon").first()
 }
