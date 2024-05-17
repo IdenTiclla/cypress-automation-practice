20b2c19607f569e564f22b6393d38128efedd051
IdenTiclla <iden.ticlla@gmail.com>
2024-05-17 10:18:43 -0400

    improving test for testing the shopping cart page with no items.

--- cypress/pages/ShoppingCartPage.js
+++ cypress/pages/ShoppingCartPage.js
@@ -1,5 +1,5 @@
 class ShoppingCartPage {
-    getIcon = () => cy.get("div#content > i")
+    getWarningIcon = () => cy.get("div#content > i[class*='warning']")
     getTitle = () => cy.get("div#content > h1")
     getMessage = () => cy.get("div#content > p")
     getContinueButton = () => cy.get("div#content >div > a[href*='common/home']")
