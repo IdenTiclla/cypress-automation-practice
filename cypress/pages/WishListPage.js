e37f548824f1834b1b7f8cfd3943dd20652fd0cb
IdenTiclla <iden.ticlla@gmail.com>
2024-05-24 07:43:54 -0400

    adding test for testing the remove product action on wish list page.

--- cypress/pages/WishListPage.js
+++ cypress/pages/WishListPage.js
@@ -1,8 +1,10 @@
+import Alert from "../e2e/components/Alert"
 import RightNavigationBar from "../e2e/components/RightNavigationBar"
 
 class WishListPage {
     constructor() {
         this.rightNavigationComponent = new RightNavigationBar
+        this.alertComponent = new Alert
     }
     getProducts = () => cy.get("table.table.table-hover.border > tbody > tr")
 }
