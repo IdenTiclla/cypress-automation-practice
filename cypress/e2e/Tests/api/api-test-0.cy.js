4fa79b59357b040046eba4745a03130fc1df2f80
IdenTiclla <iden.ticlla@gmail.com>
2024-07-02 12:44:44 -0400

    adding test for checking the amount of propery of an object

--- cypress/e2e/Tests/api/api-test-0.cy.js
+++ cypress/e2e/Tests/api/api-test-0.cy.js
@@ -16,13 +16,23 @@ describe("Test for testing an api.", () => {
             expect(response.headers).to.have.property('vary', "Origin, Accept-Encoding")
         })
     })
-    it.only("Test 2 - Checking the amount of data on response.", () => {
+    it("Test 2 - Checking the amount of data on response.", () => {
         cy.request({
             method: 'GET',
             url: 'https://jsonplaceholder.typicode.com/comments'
         }).then(response => {
             expect(response.body).to.have.length(500)
             cy.wrap(response.body).should('have.length', 500)
+        })
+    })
+    it.only("Test 3 - Checking the amount of property of every of an object", () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://jsonplaceholder.typicode.com/comments'
+        }).then(response => {
+            const firstObject = response.body[0]
+            expect(Object.keys(firstObject)).to.have.length(5)
+            cy.wrap(Object.keys(firstObject)).should('have.length', 5)
         })
     })
 })
