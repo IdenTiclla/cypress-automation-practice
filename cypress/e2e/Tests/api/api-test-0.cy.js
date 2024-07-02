0a8698ee6ff80a9bd79faddf9c4ffb33c899cd10
IdenTiclla <iden.ticlla@gmail.com>
2024-07-02 12:39:32 -0400

    adding api test for checking the amount of data on response.

--- cypress/e2e/Tests/api/api-test-0.cy.js
+++ cypress/e2e/Tests/api/api-test-0.cy.js
@@ -7,13 +7,22 @@ describe("Test for testing an api.", () => {
             expect(response.status).to.eq(200)
         })
     })
-    it.only("Test 1 - Checking headers values.", () => {
+    it("Test 1 - Checking headers values.", () => {
         cy.request({
             method: 'GET',
             url: 'https://jsonplaceholder.typicode.com/comments'
         }).then(response => {
             expect(response.headers).to.have.property('content-type', "application/json; charset=utf-8")
             expect(response.headers).to.have.property('vary', "Origin, Accept-Encoding")
+        })
+    })
+    it.only("Test 2 - Checking the amount of data on response.", () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://jsonplaceholder.typicode.com/comments'
+        }).then(response => {
+            expect(response.body).to.have.length(500)
+            cy.wrap(response.body).should('have.length', 500)
         })
     })
 })
